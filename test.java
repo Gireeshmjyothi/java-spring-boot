310921bc8dfede5ffe713dcae2b2d1cb149eb798
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2024-12-10 17:35:38 +0530

    Update test.java

--- test.java
+++ test.java
@@ -2,99 +2,169 @@ package com.epay.transaction.service;
 
 import com.epay.transaction.dao.TokenDao;
 import com.epay.transaction.dto.MerchantDto;
+import com.epay.transaction.dto.OrderDto;
 import com.epay.transaction.dto.TokenDto;
-import com.epay.transaction.exceptions.TransactionException;
+import com.epay.transaction.exceptions.ValidationException;
 import com.epay.transaction.model.response.TransactionResponse;
-import com.epay.transaction.util.enums.TokenType;
+import com.epay.transaction.util.enums.TokenStatus;
 import com.epay.transaction.validator.TokenValidator;
+import com.sbi.epay.authentication.model.AccessTokenRequest;
 import com.sbi.epay.authentication.service.AuthenticationService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class TokenServiceTest {
+
+    @InjectMocks
+    private TokenService tokenService;
+
+    @Mock
+    private TokenValidator validator;
 
     @Mock
     private TokenDao tokenDao;
 
     @Mock
-    private AuthenticationService authService;
+    private AuthenticationService authenticationService;
 
-    @Mock
-    private TokenValidator tokenValidator;
 
-    @InjectMocks
-    private TokenService tokenService;
+    private static final String MERCHANT_API_KEY_ID = "testKeyId";
+    private static final String MERCHANT_API_KEY_SECRET = "testKeySecret";
+    private static final String ORDER_HASH_CODE = "orderHash_code";
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
 
     @Test
-    void testGenerateToken() {
-        // Mock inputs
-        String merchantApiKeyId = "testApiKey";
-        String merchantApiKeySecret = "testSecret";
+    void testGenerateTokenSuccess() {
 
-        // Mock MerchantDto
-        MerchantDto merchantDto = new MerchantDto();
-        merchantDto.setMID("testMID");
-        merchantDto.setSecretKey("testSecretKey");
-        merchantDto.setApiKey("testApiKey");
-        merchantDto.setAccessTokenExpiryTime(30);
-
-        // Stub methods
-        doNothing().when(tokenValidator).validateAccessTokenRequest(merchantApiKeyId, merchantApiKeySecret);
-        when(tokenDao.getActiveMerchantByKeys(merchantApiKeyId, merchantApiKeySecret)).thenReturn(merchantDto);
-        when(authService.generateAccessToken(any())).thenReturn("generatedAccessToken");
-        when(tokenDao.saveToken(any())).thenAnswer(invocation -> {
-            TokenDto tokenDto = invocation.getArgument(0);
-            tokenDto.setGeneratedToken("generatedAccessToken");
-            return tokenDto;
+        doNothing().when(validator).validateAccessTokenRequest(MERCHANT_API_KEY_ID, MERCHANT_API_KEY_SECRET);
+        when(tokenDao.getActiveMerchantByKeys(MERCHANT_API_KEY_ID, MERCHANT_API_KEY_SECRET)).thenReturn(buildMerchantData());
+        when(tokenDao.saveToken(any(TokenDto.class))).thenAnswer(invocationOnMock -> {
+            TokenDto tokenDto1 = invocationOnMock.getArgument(0);
+            tokenDto1.setGeneratedToken("==fafoidvn234zxvvn");
+            tokenDto1.setTokenType("Merchant");
+            tokenDto1.setId(UUID.fromString("462c0c15-6c6c-4061-b2f2-852fa070816a"));
+            tokenDto1.setStatus("Active");
+            return tokenDto1;
         });
 
-        // Call the method under test
-        TransactionResponse<String> response = tokenService.generateToken(merchantApiKeyId, merchantApiKeySecret);
+        when(authenticationService.generateAccessToken(any(AccessTokenRequest.class))).thenReturn("==fafoidvn234zxvvn");
 
-        // Verify interactions
-        verify(tokenValidator, times(1)).validateAccessTokenRequest(merchantApiKeyId, merchantApiKeySecret);
-        verify(tokenDao, times(1)).getActiveMerchantByKeys(merchantApiKeyId, merchantApiKeySecret);
-        verify(authService, times(1)).generateAccessToken(any());
-        verify(tokenDao, times(2)).saveToken(any());
+        TransactionResponse<String> response = tokenService.generateToken(MERCHANT_API_KEY_ID, MERCHANT_API_KEY_SECRET);
 
-        // Assertions
+        verify(validator, times(1)).validateAccessTokenRequest(MERCHANT_API_KEY_ID, MERCHANT_API_KEY_SECRET);
+        verify(tokenDao, times(1)).getActiveMerchantByKeys(MERCHANT_API_KEY_ID, MERCHANT_API_KEY_SECRET);
+        verify(tokenDao, times(2)).saveToken(any(TokenDto.class));
+
         assertNotNull(response);
-        assertEquals(1, response.getStatus());
-        assertEquals("generatedAccessToken", response.getData().get(0));
+        assertEquals("==fafoidvn234zxvvn", response.getData().get(0));
     }
 
     @Test
-    void testGenerateTokenWithInvalidMerchant() {
-        // Mock inputs
-        String merchantApiKeyId = "invalidApiKey";
-        String merchantApiKeySecret = "invalidSecret";
+    void testGenerateTokenForValidationException() {
 
-        // Stub methods
-        doNothing().when(tokenValidator).validateAccessTokenRequest(merchantApiKeyId, merchantApiKeySecret);
-        when(tokenDao.getActiveMerchantByKeys(merchantApiKeyId, merchantApiKeySecret))
-            .thenThrow(new TransactionException("404", "Merchant not found"));
+        String merchantApiKeyId = null;
+        String merchantApiKeySecret = "invalidMerchantAPiKeySecret";
+        doThrow(new ValidationException("1001", "Invalid Api Key")).when(validator).validateAccessTokenRequest(merchantApiKeyId, merchantApiKeySecret);
 
-        // Call the method under test and assert exception
-        TransactionException exception = assertThrows(TransactionException.class, () ->
-            tokenService.generateToken(merchantApiKeyId, merchantApiKeySecret)
-        );
+        ValidationException validationException = assertThrows(ValidationException.class,
+                () -> validator.validateAccessTokenRequest(merchantApiKeyId, merchantApiKeySecret));
+        assertNotNull(validationException);
+        assertEquals("1001", validationException.getErrorCode());
+        assertEquals("Invalid Api Key", validationException.getErrorMessage());
+    }
 
-        // Verify interactions
-        verify(tokenValidator, times(1)).validateAccessTokenRequest(merchantApiKeyId, merchantApiKeySecret);
-        verify(tokenDao, times(1)).getActiveMerchantByKeys(merchantApiKeyId, merchantApiKeySecret);
+    @Test
+    public void testGenerateTransactionTokenSuccess() {
 
-        // Assertions
-        assertEquals("404", exception.getErrorCode());
-        assertEquals("Merchant not found", exception.getMessage());
+        when(tokenDao.getActiveTransactionByHashValue(ORDER_HASH_CODE)).thenReturn(buildOrderDto());
+        when(tokenDao.getActiveMerchantByMID(buildOrderDto().getMId())).thenReturn(buildMerchantData());
+
+        when(tokenDao.saveToken(any(TokenDto.class))).thenAnswer(invocationOnMock -> {
+            TokenDto tokenDto1 = invocationOnMock.getArgument(0);
+            tokenDto1.setGeneratedToken("==fafoidvn234zxvvn");
+            tokenDto1.setTokenType("Transaction");
+            tokenDto1.setId(UUID.fromString("462c0c15-6c6c-4061-b2f2-852fa070816a"));
+            tokenDto1.setStatus("Active");
+            return tokenDto1;
+        });
+
+        TransactionResponse<String> transactionResponse = tokenService.generateTransactionToken(ORDER_HASH_CODE);
+
+        verify(tokenDao, times(1)).getActiveTransactionByHashValue(ORDER_HASH_CODE);
+        verify(tokenDao, times(1)).getActiveMerchantByMID(buildOrderDto().getMId());
+        verify(tokenDao, times(2)).saveToken(any(TokenDto.class));
+
+        assertNotNull(transactionResponse);
+        assertEquals("==fafoidvn234zxvvn", transactionResponse.getData().get(0));
+
+
+    }
+
+    @Test
+    public void testGeneratePaymentTokenSuccess() {
+
+        when(tokenDao.getActiveTransactionByHashValue(ORDER_HASH_CODE)).thenReturn(buildOrderDto());
+        when(tokenDao.getActiveMerchantByMID(buildOrderDto().getMId())).thenReturn(buildMerchantData());
+
+        when(tokenDao.saveToken(any(TokenDto.class))).thenAnswer(invocationOnMock -> {
+            TokenDto tokenDto1 = invocationOnMock.getArgument(0);
+            tokenDto1.setGeneratedToken("generated_token");
+            tokenDto1.setTokenType("Payment");
+            tokenDto1.setId(UUID.fromString("5f310393-7949-4ea7-9e01-85cc550d528b"));
+            tokenDto1.setStatus("Active");
+            return tokenDto1;
+        });
+
+        TransactionResponse<String> transactionResponse = tokenService.generatePaymentToken(ORDER_HASH_CODE);
+
+        verify(tokenDao, times(1)).getActiveTransactionByHashValue(ORDER_HASH_CODE);
+        verify(tokenDao, times(1)).getActiveMerchantByMID(buildOrderDto().getMId());
+        verify(tokenDao, times(2)).saveToken(any(TokenDto.class));
+
+        assertNotNull(transactionResponse);
+        assertEquals("generated_token", transactionResponse.getData().get(0));
+
+    }
+
+
+    private MerchantDto buildMerchantData() {
+        return MerchantDto.builder()
+                .mID("Merchant_id")
+                .aek("aek_key")
+                .kek("kek_key")
+                .mek("mek_key")
+                .accessTokenExpiryTime(24)
+                .apiKey("api_key")
+                .secretKey("secret_key")
+                .status(TokenStatus.ACTIVE.name())
+                .transactionTokenExpiryTime(24)
+                .accessTokenExpiryTime(24)
+                .build();
+    }
+
+    private OrderDto buildOrderDto(){
+        return OrderDto.builder()
+                .id(UUID.randomUUID())
+                .currencyCode("INR")
+                .orderHash("asfaah535")
+                .customerId("57-43715_CustId")
+                .expiry(24)
+                .mId("merchant_id")
+                .multiAccounts("No")
+                .orderAmount(459.4)
+                .orderRefNumber(35325775+"oder")
+                .otherDetails("details")
+                .sbiOrderRefNumber("sbi_3425435")
+                .paymentMode("CARD")
+                .build();
     }
 }
